dc055f8cc36405bd926a2d508f910c34e8a04de0
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-04-30 21:02:46 +0530

    Delete config.js

--- server/graphql/src/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-  module.exports = {
-	ACCESS_KEY_ID:process.env.ACCESS_KEY_ID,
-        SECRET_KEY:process.env.SECRET_KEY,
-        REGION:process.env.REGION,
-        LEDGER_NAME:process.env.LEDGER_NAME
-       }
