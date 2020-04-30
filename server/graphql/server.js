aae4e9d338ce4f4e8552c3c304f55c9aa0a47d04
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-04-30 20:45:29 +0530

    graphql server setup

---
+++ server/graphql/server.js
@@ -0,0 +1,13 @@
+const { GraphQLServer, PubSub } = require('graphql-yoga')
+
+const resolvers = require("./src/resolvers");
+const typeDefs = require("./src/typeDefs");
+const pubsub = new PubSub();
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+  context: {
+    pubsub
+  }
+});
+server.start(() => console.log('Server is running on localhost:4000'))
