45ea75f0eb6be170a46e9973739a74da0935ec3d
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-04-30 21:05:36 +0530

    Update server.js

--- server/rest/server.js
+++ server/rest/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
-const qldb_config = require('./config.js');
+const qldb_config = require('../../config.js');
 const qldb = require('qldb').default;
 const ionize = require('qldb').ionize;
 
